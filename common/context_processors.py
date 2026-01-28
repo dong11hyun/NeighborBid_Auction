0a573c3d9dd4d6d208a9fe3746f6499e952e5d8d
전대원 <monst7943@gmail.com>
2026-01-28 19:57:45 +0900

    fix: 지웠던문장추가

---
+++ common/context_processors.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+
+def global_settings(request):
+    """
+    모든 템플릿에서 공통으로 사용할 변수를 반환합니다.
+    (예: Google Maps API Key, 사이트 이름 등)
+    """
+    return {
+        'GOOGLE_MAPS_API_KEY': getattr(settings, 'GOOGLE_MAPS_API_KEY', ''),
+    }
--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'common.context_processors.global_settings', # 추가
+                'common.context_processors.global_settings',
             ],
         },
     },
